92cff7ca5af4ce38e00c3dc0156fe9e123ae377c
Hrant Nurijanyan <hrant.nurijanyan@gmail.com>
2022-11-08 00:59:44 +0400

    New approach on generating project with templates

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ chmod +x generator.py
 
 mkdir -p ~/.bin >/dev/null 2>&1
 
+mkdir -p ~/.local/share/cmaker/ >/dev/null 2>&1
+
+cp -r templates ~/.local/share/cmaker/
+
 cp generator.py ~/.bin/cmaker
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
 rm ~/.bin/cmaker 2> /dev/null
+
+rm -r ~/local/share/cmaker 2> /dev/null
